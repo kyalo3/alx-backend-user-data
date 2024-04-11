154056532e906065f9498a9c86a18387c8810985
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-12 01:22:04 +0300

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,9 @@ import mysql.connector
 import os
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 def filter_datum(
         fields: List[str],
         redaction: str, message: str, separator: str) -> str:
@@ -31,10 +34,17 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
+        """ init method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
-
-
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR)
+        return redacted
