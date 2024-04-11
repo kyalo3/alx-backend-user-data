74b261ef2107aff8ae12e3afa146bc6eb1f49482
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-12 01:15:49 +0300

    main file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,3 +21,20 @@ def filter_datum(
         message = re.sub(field + '=.*?' + separator,
                          field + '=' + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
