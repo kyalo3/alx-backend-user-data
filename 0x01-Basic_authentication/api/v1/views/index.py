dd5609e0a0b23648f6ea6eff23575e779e4eca92
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-17 16:01:50 +0300

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app.views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'])
 def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
