ac8e9de6da93550fc7a7082bdd2f61ddd3f7d6ca
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-17 17:46:20 +0300

    2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,8 +26,17 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({
-        "error": "unauthorized"
+        "error": "Unauthorized"
     }), 401
+
+
+@app.errorhandler(403)
+def not_found(error) -> str:
+    """new error handler
+    """
+    return jsonify({
+        "error": "Forbidden"
+    }), 403
 
 
 if __name__ == "__main__":
