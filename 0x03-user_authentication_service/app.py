171a21f3712682f0f787d47e24940a4cfca29f2a
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-23 13:34:03 +0300

    basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/GET')
+def create_app():
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
