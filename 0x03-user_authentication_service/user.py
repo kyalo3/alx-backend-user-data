64d57570f3a00813aff83d215867e839878116a6
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-23 14:54:22 +0300

    new task

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,13 +3,15 @@
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
-# Define Base
 Base = declarative_base()
 
 
 class User(Base):
+    """User class
+    """
     __tablename__ = 'users'
 
+    """columns"""
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
