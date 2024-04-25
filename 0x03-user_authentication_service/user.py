06aa187c250afd04f46d8cc84c9901f086404d8f
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-25 18:21:24 +0300

    update user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 
 Base = declarative_base()
 """
