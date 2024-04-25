0810a23969c6ce397960af53a106ec1b85b344f3
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-25 18:32:40 +0300

    new user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,8 +4,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-"""
-"""
+
 
 class User(Base):
     """User class
