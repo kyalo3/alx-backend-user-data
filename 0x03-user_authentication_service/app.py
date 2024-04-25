24cac1482954a5dee246230d71d9205d41282b65
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-26 00:49:37 +0300

    status code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,51 @@ def login() -> str:
             return response
 
 
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """ logout a user"""
+    try:
+        session_id = request.cookie.get("session_id")
+        user = AUTH.get_user_from_session(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+    except NoResultFound:
+        abort(403)
+
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def get_profile() -> str:
+    """ return the user based on session id"""
+    try:
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+        abort(403)
+    except NoResultFound:
+        return None
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """ function to update the password"""
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+
+        user = AUTH.find_user_by(email=email)
+        if user:
+            if user.reset_token == reset_token:
+                AUTH.update_password(reset_token, new_password)
+                return jsonify(
+                    {"email": user.email, "message": "Password updated"})
+    except ValueError:
+        return None
+    except NoResultFound:
+        return None
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
