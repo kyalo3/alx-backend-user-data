9eea6846c792e0fe1996eb38b6c426b22641c1b6
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-26 00:28:40 +0300

    new changes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,7 +46,7 @@ class DB:
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound
+                raise NoResultFound("No results found")
             return user
         except NoResultFound:
             raise NoResultFound("No results found")
