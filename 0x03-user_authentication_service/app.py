e7a6a6b45b366ad63b8f1466046ca3afa6f3b899
Kyalo3 <kyalokimeu0@gmail.com>
2024-04-25 16:57:50 +0300

    user model

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,8 +4,10 @@ create a simple flask app
 """
 from flask import Flask, jsonify, request, abort
 from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -16,6 +18,22 @@ def create_app() -> str:
 
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/', methods=['POST'], strict_slashes=False)
+def status():
+    """
+    Return:
+        - json payload
+    """
+    if user:
+        return jsonify({
+            "email": "<registered email>",
+            "message": "user created"
+        })
+
+    else:
+        return jsonify({"message": "email already registered"}), 4
 
 
 if __name__ == "__main__":
